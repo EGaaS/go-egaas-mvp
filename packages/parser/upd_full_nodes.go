a96ac68c45531d1ef8abd8e50f562afb58502c5e
c-darwin <darwin84@yandex.ru>
2016-11-02 22:13:57 +0300

    changed delegating rules

--- packages/parser/upd_full_nodes.go
+++ packages/parser/upd_full_nodes.go
@@ -120,11 +120,11 @@ func (p *Parser) UpdFullNodes() error {
 	}
 
 	where := ``
-	if p.BlockData.BlockId > 3000 {
-		where = ` AND amount > 1000`
+	if p.BlockData.BlockId > 23900 {
+		where = ` AND amount > 1000000000000000000000`
 	}
 	// получаем новые данные по wallet-нодам
-	all, err := p.GetList(`SELECT address_vote FROM dlt_wallets WHERE address_vote !='' `+where+` GROUP BY address_vote ORDER BY sum(amount) DESC LIMIT 10`).String()
+	all, err := p.GetList(`SELECT address_vote FROM dlt_wallets WHERE address_vote !='' `+where+` GROUP BY address_vote ORDER BY sum(amount) DESC LIMIT 100`).String()
 	if err != nil {
 		return p.ErrInfo(err)
 	}
