a94c82589ddd01ac04a5bf4a2ce901628f135419
c-darwin <darwin84@yandex.ru>
2017-09-06 19:44:45 +0300

    Update new_state.go

--- packages/parser/new_state.go
+++ packages/parser/new_state.go
@@ -271,14 +271,32 @@ IfEnd:
 PageEnd:
 `, `menu_default`, sid,
 
-		`government`, `FullScreen(1)
-Divs(md-4, panel panel-default elastic data-sweet-alert)
-Divs(panel-body)
-BtnPage(sys-listOfTables, Tables)
-BtnPage(sys-contracts, Contracts)
-BtnPage(sys-interface, Interface)
-DivsEnd:
-DivsEnd:
+		`government`, `
+FullScreen(1)
+If(StateVal(tokens_accounts_type,1))
+Else:
+Title : Basic Apps
+Divs: col-md-4
+  Divs: panel panel-default elastic
+   Divs: panel-body text-center fill-area flexbox-item-grow
+    Divs: flexbox-item-grow flex-center
+     Divs: pv-lg
+     Image("/static/img/apps/money.png", Basic, center-block img-responsive img-circle img-thumbnail thumb96 )
+     DivsEnd:
+     P(h4,Basic Apps)
+     P(text-left,"Election and Assign, Polling, Messenger, Simple Money System")
+    DivsEnd:
+   DivsEnd:
+   Divs: panel-footer
+    Divs: clearfix
+     Divs: pull-right
+      BtnPage(app-basic, Install,'',btn btn-primary lang)
+     DivsEnd:
+    DivsEnd:
+   DivsEnd:
+  DivsEnd:
+ DivsEnd:
+IfEnd:
 PageEnd:
 `, `government`, sid,
 	)
