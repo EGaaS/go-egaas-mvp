145e0579604610b02a6bb6fe4915253944360735
gentee <akrivonogov@gmail.com>
2017-09-07 09:40:12 +0500

    Fixed FirstBlock problem

--- packages/parser/common_parse_transaction.go
+++ packages/parser/common_parse_transaction.go
@@ -18,7 +18,10 @@ package parser
 
 import (
 	"fmt"
+	"reflect"
+
 	"github.com/EGaaS/go-egaas-mvp/packages/consts"
+	"github.com/EGaaS/go-egaas-mvp/packages/lib"
 	"github.com/EGaaS/go-egaas-mvp/packages/utils"
 )
 
@@ -32,9 +35,65 @@ func (p *Parser) ParseTransaction(transactionBinaryData *[]byte) ([][]byte, erro
 
 		// хэш транзакции
 		transSlice = append(transSlice, utils.DSha256(*transactionBinaryData))
+		input := (*transactionBinaryData)[:]
 
 		// первый байт - тип транзакции
 		txType := utils.BinToDecBytesShift(transactionBinaryData, 1)
+		isStruct := consts.IsStruct(int(txType))
+		if txType > 127 { // транзакция с контрактом
+				var err error
+				p.TxPtr = &consts.TXHeader{}
+					if err = lib.BinUnmarshal(&input, p.TxPtr); err != nil {
+						return nil, err
+					}
+					isStruct = false
+					p.TxStateID = uint32(p.TxPtr.(*consts.TXHeader).StateId)
+					p.TxStateIDStr = utils.UInt32ToStr(p.TxStateID)
+					if p.TxStateID > 0 {
+						p.TxCitizenID = int64(p.TxPtr.(*consts.TXHeader).WalletId)
+						p.TxWalletID = 0
+					} else {
+						p.TxCitizenID = 0
+						p.TxWalletID = int64(p.TxPtr.(*consts.TXHeader).WalletId)
+					}
+					/*contract := smart.GetContractById(p.TxPtr.(*consts.TXHeader).Type)
+					if contract == nil {
+						return nil, fmt.Errorf(`unknown contract %d`, p.TxPtr.(*consts.TXHeader).Type)
+					}*/
+					forsign := fmt.Sprintf("%d,%d,%d,%d,%d", p.TxPtr.(*consts.TXHeader).Type,
+						p.TxPtr.(*consts.TXHeader).Time, p.TxPtr.(*consts.TXHeader).WalletId,
+						p.TxPtr.(*consts.TXHeader).StateId, p.TxPtr.(*consts.TXHeader).Flags)
+		
+					//p.TxContract = contract
+					p.TxData = make(map[string]interface{})
+					p.TxData[`forsign`] = forsign
+					//			fmt.Println(`Contract data`, p.TxData)
+			} else if isStruct {
+					p.TxPtr = consts.MakeStruct(consts.TxTypes[int(txType)])
+					if err := lib.BinUnmarshal(&input, p.TxPtr); err != nil {
+						return nil, err
+					}
+					p.TxVars = make(map[string]string)
+					if int(txType) == 4 { // TXNewCitizen
+						head := consts.HeaderNew(p.TxPtr)
+						p.TxStateID = uint32(head.StateId)
+					p.TxStateIDStr = utils.UInt32ToStr(p.TxStateID)
+						if head.StateId > 0 {
+							p.TxCitizenID = int64(head.WalletId)
+							p.TxWalletID = 0
+						} else {
+							p.TxCitizenID = 0
+							p.TxWalletID = int64(head.WalletId)
+					}
+						p.TxTime = int64(head.Time)
+					} else {
+					head := consts.Header(p.TxPtr)
+						p.TxCitizenID = head.CitizenId
+						p.TxWalletID = head.WalletId
+						p.TxTime = int64(head.Time)
+					}
+//					fmt.Println(`PARSED STRUCT %v`, p.TxPtr)
+				}		
 		transSlice = append(transSlice, utils.Int64ToByte(txType))
 		if len(*transactionBinaryData) == 0 {
 			return transSlice, utils.ErrInfo(fmt.Errorf("incorrect tx"))
@@ -45,33 +104,52 @@ func (p *Parser) ParseTransaction(transactionBinaryData *[]byte) ([][]byte, erro
 			return transSlice, utils.ErrInfo(fmt.Errorf("incorrect tx"))
 		}
 		log.Debug("%s", transSlice)
+		if txType > 127 {
+						*transactionBinaryData = (*transactionBinaryData)[len(*transactionBinaryData):]
+				} else if isStruct {
+						t := reflect.ValueOf(p.TxPtr).Elem()
+			
+						//walletId & citizenId
+						for i := 2; i < 4; i++ {
+							data := lib.FieldToBytes(t.Field(0).Interface(), i)
+							returnSlice = append(returnSlice, data)
+						}
+						for i := 1; i < t.NumField(); i++ {
+							data := lib.FieldToBytes(t.Interface(), i)
+							returnSlice = append(returnSlice, data)
+						}
+					} else {		
 		// преобразуем бинарные данные транзакции в массив
-			i := 0
-			for {
-				length := utils.DecodeLength(transactionBinaryData)
-				i++
+		i := 0
+		for {
+			length := utils.DecodeLength(transactionBinaryData)
+			i++
+			log.Debug("%v", i)
+			if i >= 20 { // у нас нет тр-ий с более чем 20 элементами
 				log.Debug("%v", i)
-				if i >= 20 { // у нас нет тр-ий с более чем 20 элементами
-					log.Debug("%v", i)
-					break
-				}
-				log.Debug("%v", length)
-				if length > 0 && length < consts.MAX_TX_SIZE {
-					data := utils.BytesShift(transactionBinaryData, length)
-					returnSlice = append(returnSlice, data)
-					log.Debug("%x", data)
-					log.Debug("%s", data)
-				} else if length == 0 && len(*transactionBinaryData) > 0 {
-					log.Debug("continue")
-					returnSlice = append(returnSlice, []byte{})
-					continue
-				}
-				if length == 0  { // у нас нет тр-ий
-					log.Debug("length == 0")
-					break
-				}
+				break
 			}
-			log.Debug("end")
+			log.Debug("%v", length)
+			if length > 0 && length < consts.MAX_TX_SIZE {
+				data := utils.BytesShift(transactionBinaryData, length)
+				returnSlice = append(returnSlice, data)
+				log.Debug("%x", data)
+				log.Debug("%s", data)
+			} else if length == 0 && len(*transactionBinaryData) > 0 {
+				log.Debug("continue")
+				returnSlice = append(returnSlice, []byte{})
+				continue
+			}
+			if length == 0 { // у нас нет тр-ий
+				log.Debug("length == 0")
+				break
+			}
+		}
+	}
+			if isStruct {
+				*transactionBinaryData = (*transactionBinaryData)[len(*transactionBinaryData):]
+			}		
+		log.Debug("end")
 		if len(*transactionBinaryData) > 0 {
 			return transSlice, utils.ErrInfo(fmt.Errorf("incorrect transactionBinaryData %x", transactionBinaryData))
 		}
